7ad9a29543c6ca17b457bf74c2c095924b64c57a
spragala <spragala@yahoo.com>
2017-05-19 17:04:00 -0700

    picture loads in thumbnail

--- scripts/app.js
+++ scripts/app.js
@@ -12,20 +12,33 @@ $(document).on("ready", function() {
       method: "GET",
       url: endpoint,
       data: $("input").serialize(),
-      success: function(response) {
-
+      //success: function(response) {
+				success: function(response) {
+					let pic 	= response.data[0].images.fixed_height_small.url;
+					$(".thumbnail").append(`<img src="${pic}"/>`)
+				}
 // RESET HTML TO APPEND BLANK PAGE
-        $('.gif-gallery').html("");
+        //$('.gif-gallery').html("");
 
 // DISPLAY PICS
-        gifArray = response.data
-				
-        gifArray.forEach(function(image) {
-          let pic = image.images.fixed_height_small.url
-          $('.gif-gallery').append(`<img src="${pic}"/>`);
-        })
-      }
+        //gifArray = response.data
+
+      //  gifArray.forEach(function(image) {
+        //  let pic = image.images.fixed_height_small.url
+        //  $('.gif-gallery').append(`<img src="${pic}"/>`);
+        //})
+    //  }
     })
   });
 
 });
+
+
+// pseduocode - MAKE THE DAMN THING PRETTY!
+
+/* 	1. Create thumbnail template - start with 2
+		2. Create id's for thumbnail
+		3. Grab title and slug from Gify API
+		4. Add the info into the specific thumbnail spots
+
+*/
